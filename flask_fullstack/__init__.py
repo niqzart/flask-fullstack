a936b3b9747775552885ad8945406588d81f63b6
qzart45hi <qwert45hi@yandex.ru>
2022-02-08 23:12:02 +0300

    feat: externalize testing utils from xieffect

--- flask_fullstack/__init__.py
+++ flask_fullstack/__init__.py
@@ -4,7 +4,8 @@ from .interfaces import Identifiable, UserRole
 from .marshals import LambdaFieldDef, Marshalable, ResponseDoc, create_marshal_model, unite_models, DateTimeField
 from .mixins import AbstractAbortMixin, DatabaseSearcherMixin, JWTAuthorizerMixin
 from .parsers import counter_parser, password_parser
+from .pytest import check_code
 from .restx import RestXNamespace
 from .sqlalchemy import Sessionmaker, JSONWithModel
-from .utils import TypeEnum, get_or_pop
+from .utils import TypeEnum, get_or_pop, dict_equal
 from .whoosh import IndexService, Searcher
--- flask_fullstack/utils.py
+++ flask_fullstack/utils.py
@@ -23,3 +23,9 @@ class TypeEnum(Enum):
 
 def get_or_pop(dictionary: dict, key, keep: bool = False):
     return dictionary[key] if keep else dictionary.pop(key)
+
+
+def dict_equal(dict1: dict, dict2: dict, *keys) -> bool:
+    dict1 = {key: dict1.get(key, None) for key in keys}
+    dict2 = {key: dict2.get(key, None) for key in keys}
+    return dict1 == dict2
