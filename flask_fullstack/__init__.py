d393d9ca6159f83acdde30a1d4527a7719bcd44c
qzart45hi <qwert45hi@yandex.ru>
2022-03-02 11:23:33 +0300

    feat: Undefined instead of None for absent arguments

--- flask_fullstack/__init__.py
+++ flask_fullstack/__init__.py
@@ -5,7 +5,7 @@ from .marshals import LambdaFieldDef, Marshalable, ResponseDoc, create_marshal_m
 from .mixins import AbstractAbortMixin, DatabaseSearcherMixin, JWTAuthorizerMixin
 from .parsers import counter_parser, password_parser
 from .pytest import check_code
-from .restx import RestXNamespace
+from .restx import RestXNamespace, Undefined
 from .sqlalchemy import Sessionmaker, JSONWithModel
 from .utils import TypeEnum, get_or_pop, dict_equal
 from .whoosh import IndexService, Searcher
