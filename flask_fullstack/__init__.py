37871e8476fd868bcc0ac148090b73245c5cf35f
qzart45hi <qwert45hi@yandex.ru>
2022-03-28 11:51:38 +0300

    feat: add new API to __init__

--- flask_fullstack/__init__.py
+++ flask_fullstack/__init__.py
@@ -1,7 +1,8 @@
 from .core import Flask, configure_logging, configure_whooshee, configure_sqlalchemy
 from .eventor import EventGroup, BaseEventGroup, Namespace
 from .interfaces import Identifiable, UserRole
-from .marshals import LambdaFieldDef, Marshalable, ResponseDoc, create_marshal_model, unite_models, DateTimeField
+from .marshals import ResponseDoc, DateTimeField, Model, PydanticModel
+from .marshals import LambdaFieldDef, Marshalable, create_marshal_model, unite_models  # DEPRECATED
 from .mixins import AbstractAbortMixin, DatabaseSearcherMixin, JWTAuthorizerMixin
 from .parsers import counter_parser, password_parser
 from .pytest import check_code
