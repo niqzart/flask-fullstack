1b148cc09e9f250b52bb77e155bf071e8d5910e4
qzart45hi <qwert45hi@yandex.ru>
2022-04-20 02:27:37 +0300

    fix: replace not working name overriding with a TODO

--- flask_fullstack/utils.py
+++ flask_fullstack/utils.py
@@ -38,7 +38,7 @@ class Nameable:
 class NamedPropertiesMeta(type):
     def __init__(cls, name: str, bases: tuple[type, ...], namespace: dict[str, ...]):
         for name, value in namespace.items():
-            if isinstance(value, type) and issubclass(value, Nameable) and value.name is None:
+            if isinstance(value, type) and issubclass(value, Nameable):  # TODO ability to override names
                 value.name = namespace["__qualname__"] + "." + name
         super().__init__(name, bases, namespace)
 
