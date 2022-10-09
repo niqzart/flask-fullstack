0f2916ab8ac7c93b81a9312c0ee373f781165285
qzart45hi <qwert45hi@yandex.ru>
2022-10-09 21:40:20 +0300

    feat: add setup.py & update requirements

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name="Flask Fullstack",
+    version="0.4.2",
+    description="TBA",
+    author="niqzart",
+    author_email="qwert45hi@yandex.ru",
+    url="https://github.com/niqzart/flask-fullstack",
+    packages=["flask_fullstack"],
+    install_requires=[
+        "Flask-Cors ~= 3.0.10",
+        "Flask-JWT-Extended ~= 4.3.1",
+        "Flask-RESTX ~= 0.5.1",
+        "Flask-SocketIO ~= 5.1.0",
+        "pydantic ~= 1.9.0",
+        "SQLAlchemy ~= 1.4.31",
+        "Werkzeug == 2.0.2",  # for restx
+        "Whoosh ~= 2.7.4",
+        "WhooshAlchemy ~= 0.3.1",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ],
+    },
+)
