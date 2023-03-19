6b5fcf15df9bc9db89e7645c3824ce3c08023ebb
niqzart <qwert45hi@yandex.ru>
2023-03-19 12:39:09 +0300

    feat: pydantic-based equivalency check (contains)

--- flask_fullstack/__init__.py
+++ flask_fullstack/__init__.py
@@ -5,6 +5,6 @@ from .restx import ResourceController, Undefined
 from .restx import ResponseDoc, DateTimeField, Model, PydanticModel
 from .siox import ClientEvent, ServerEvent, DuplexEvent, EventException
 from .siox import EventController, EventSpace, Namespace, SocketIO
-from .utils import check_code, get_or_pop, dict_equal, NotImplementedField, TypeEnum
+from .utils import assert_contains, TypeChecker
 from .utils import IndexService, Searcher, SQLAlchemy, JSONWithSchema, JSONWithModel
 from .utils import NamedProperties, NamedPropertiesMeta, Nameable
